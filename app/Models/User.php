16fb7b06f9db79332c4868949d7263190eb3325c
gurwan <gurwan.thomas56190@gmail.com>
2026-03-04 10:32:33 +0100

    6

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,11 +6,14 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use LdapRecord\Laravel\Auth\AuthenticatesWithLdap;
 
 class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
+
+    use AuthenticatesWithLdap;
 
     /**
      * The attributes that are mass assignable.
