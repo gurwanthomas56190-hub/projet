777290e9e617a3f087053dc300a4a74623be86a2
gurwan <gurwan.thomas56190@gmail.com>
2026-03-24 13:54:42 +0100

    authV2.1

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -7,27 +7,14 @@ use LdapRecord\Laravel\Middleware\WindowsAuthenticate;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
     public function register(): void
     {
         //
     }
 
-    /**
-     * Bootstrap any application services.
-     */
     public function boot(): void
     {
-        // On indique le nom du header configuré dans Nginx
-        // Note : PHP transforme "X-Remote-User" en "HTTP_X_REMOTE_USER"
+        // On définit la clé de serveur pour lire ce que Nginx envoie
         WindowsAuthenticate::serverKey('HTTP_X_REMOTE_USER');
-
-        // Nettoyage du login : Si Nginx envoie "user@SILVADEC.LOCAL", 
-        // on ne garde que "user" pour la recherche dans l'AD.
-        WindowsAuthenticate::retrieveUserWith(function ($user) {
-            return explode('@', $user)[0];
-        });
     }
 }
