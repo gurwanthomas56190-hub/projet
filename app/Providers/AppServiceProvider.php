111c89012a6276f37589ac7880343189a203acbb
gurwan <gurwan.thomas56190@gmail.com>
2026-04-30 17:03:40 +0200

    fichiersv3.01

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,37 +1,24 @@
 <?php
 
-namespace App\Http\Middleware;
+namespace App\Providers;
 
-use Closure;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
-use App\Ldap\User as LdapUser;
+use Illuminate\Support\ServiceProvider;
 
-class KerberosSSO
+class AppServiceProvider extends ServiceProvider
 {
-    public function handle(Request $request, Closure $next)
+    /**
+     * Register any application services.
+     */
+    public function register(): void
     {
-        // On ne fait ça que si l'utilisateur n'est pas déjà connecté
-        if (!Auth::check()) {
-            
-            // On lit l'en-tête envoyé par Nginx
-            $remoteUser = $request->header('X-Remote-User');
-            
-            if ($remoteUser) {
-                // On nettoie le nom (ex: gurwan@SILVADEC.LOCAL -> gurwan)
-                $username = explode('@', $remoteUser)[0];
+        //
+    }
 
-                // On cherche l'utilisateur dans l'Active Directory
-                $user = LdapUser::where('samaccountname', $username)->first();
-                
-                // Si on le trouve, on le connecte silencieusement !
-                if ($user) {
-                    Auth::login($user);
-                }
-            }
-        }
-
-        // On laisse la requête continuer vers la vraie page web
-        return $next($request);
+    /**
+     * Bootstrap any application services.
+     */
+    public function boot(): void
+    {
+        //
     }
 }
