61b3cf1079a7142eac1f3004c3234659f3a14fc5
gurwan <gurwan.thomas56190@gmail.com>
2026-05-04 09:20:24 +0200

    annuairev3.1

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -17,8 +17,12 @@ class AppServiceProvider extends ServiceProvider
     {
         // On vérifie si l'utilisateur connecté fait partie du groupe des admins de l'intranet dans l'AD
         Gate::define('gerer-annuaire', function ($user) {
-            // On récupère l'utilisateur LDAP correspondant à l'utilisateur connecté
-            $ldapUser = LdapUser::where('samaccountname', $user->samaccountname)->first();
+            
+            // Sécurité : On s'assure d'extraire la chaîne de caractères (string) si l'attribut est un tableau
+            $username = is_array($user->samaccountname) ? $user->samaccountname[0] : $user->samaccountname;
+
+            // On récupère l'utilisateur LDAP correspondant à l'utilisateur connecté avec la vraie chaîne de texte
+            $ldapUser = LdapUser::where('samaccountname', $username)->first();
             
             // Remplace le DN par celui de ton groupe Admin dans ton arborescence AD Windows Server
             return $ldapUser && $ldapUser->groups()->exists('cn=Intranet_Admins,ou=Groupes,dc=silvadec,dc=local');
